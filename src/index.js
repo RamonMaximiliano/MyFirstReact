f484196b8614fa24f70eac1665e4234cb78cd814
Ramon <ramon_max1986@hotmail.com>
2022-01-10 16:54:32 -0300

    studying

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 
 
 const myfirstelement = <h1>Hello React!</h1>
-
 ReactDOM.render(myfirstelement, document.getElementById('root'));
+
+
+
+const myArray = ['apple', 'banana', 'orange'];
+const myList = myArray.map((item) => <p>{item}</p>)
+ReactDOM.render(myList, document.getElementById('root2'));
